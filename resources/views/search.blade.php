b3bd15910c48128f1225df9fcf576ae244c99dba
Sidneybruno <sidneybruno113@gmail.com>
2023-10-25 08:05:59 -0300

    pagina de busca de artigos

---
+++ resources/views/search.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>procurar artigos</title>
+    <link rel="stylesheet" href="/resources/css/stylepag.css">
+  </head>
+  <body>
+    <h1>Pesquise sobre nossas postagens</h1>
+   <div class="search-box">
+      <input type="text" class="search-text" placeholder="Pesquisar ...">
+      <a class="search-btn">
+      <img class="loupe-blue" src="/public/img/loupe-blue.svg" alt="" width="25px" height="25px">
+      <img class="loupe-white" src="/public\img/loupe-white.svg" alt="" width="25px" height="25px">
+      </a>
+   </div>
+  </body>
+</html>
